da70804de6729b974a120b212175060318f34edc
SandipNITA2025 <sarkarsandip7910@gmail.com>
2023-05-19 15:55:32 +0530

    done

---
+++ models/Post.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: false,
+    },
+    photo: {
+      public_id: {
+        type: String,
+        required: false,
+      },
+      url: {
+        type: String,
+        required: false,
+      },
+    },
+    username: {
+      type: String,
+      required: false,
+    },
+    categories: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
